c711e630e40e944427ff5af9772ea58e7b329938
Ron Berkowitz <ronberkowitz@Roys-MacBook-Pro.local>
2022-02-09 20:28:38 +0200

    finished gh workflow and added outputs to git

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,11 @@
 import requests
 import pandas as pd
+import smtplib
+from os.path import basename
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import COMMASPACE, formatdate
 
 
 def get_current_round_number() -> int:
@@ -28,3 +34,39 @@ def get_fantasy_stats_df() -> pd.DataFrame:
 
     response = requests.post('https://api.dunkest.com/api/stats', headers=headers, data=data)
     return pd.read_excel(response.content)
+
+
+
+def send_mail_notification(send_from, send_to, subject, text, files=None):
+    assert isinstance(send_to, list)
+
+    msg = MIMEMultipart()
+    msg['From'] = send_from
+    msg['To'] = COMMASPACE.join(send_to)
+    msg['Date'] = formatdate(localtime=True)
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(text))
+
+    for f in files or []:
+        with open(f, "rb") as fil:
+            part = MIMEApplication(
+                fil.read(),
+                Name=basename(f)
+            )
+        # After the file is closed
+        part['Content-Disposition'] = 'attachment; filename="%s"' % basename(f)
+        msg.attach(part)
+
+
+    #Create your SMTP session 
+    smtp = smtplib.SMTP('smtp.office365.com', 587) 
+    #Use TLS to add security 
+    smtp.starttls() 
+    
+    #User Authentication 
+    smtp.login("euroleague.fantasy.script@hotmail.com","EuroleagueFantasyScript")
+    
+    smtp.sendmail(send_from, send_to, msg.as_string())
+    smtp.quit()
+    
